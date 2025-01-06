d568985751084d86d1851ccbf2e280ef78cbc412
Sheldy Rivaldi <sheldyrivaldi@gmail.com>
2025-01-06 18:20:57 +0700

    update casesensitive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Transaksi from './Pages/Transaksi'
-import Profil from './Pages/Profil'
-import Login from './Pages/Login'
+import Home from './pages/Home'
+import Transaksi from './pages/Transaksi'
+import Profil from './pages/Profil'
+import Login from './pages/Login'
 import './App.css'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />s
         <Route path='/transaksi' element={<Transaksi />} />
         <Route path='/profil' element={<Profil />} />
         <Route path='/login' element={<Login />} />
